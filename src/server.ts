6a1fea3ee81be794915c00fa468a927a240b61df
Andrés Villalobos <agmvilla@gmail.com>
2020-09-03 01:57:12 -0300

    Aulas - Primeiro projeto com Node.js

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { response } from 'express';
+// src/server.ts
+import routes from './routes';
+import appointmentsRouter from './routes/appointment.routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.get('/', (request, response) =>
+    response.json({ message: 'Hello UMarket' }),
+);
+
+app.listen(3333, () => {
+    console.log('🚀 Server started @port 3333');
+});
---
+++ src/service/CreateAppointmentService.ts
@@ -0,0 +1,45 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * Recebimento das informções
+ * Tratativa de erros/exceções
+ * Acesso ao repositório
+ */
+
+interface Request {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({ date, provider }: Request): Appointment {
+        const appointmentDate = startOfHour(date);
+
+        console.log(this.appointmentsRepository);
+
+        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointmentInSameDate) {
+            throw Error('This appointment is already booked');
+        }
+
+        const appointment = this.appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
